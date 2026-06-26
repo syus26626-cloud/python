486b0202c6288c00ecf3a49885fcf4aeb24f1930
syus26626-cloud <syus26626@gmail.com>
2026-06-26 20:59:39 +0900

    更新 main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 def my_decorator(func):
     def wrapper(*args, **kwargs):
-        print("--- 処理開始 ---")
+        print("--- 前処理 ---")
         result = func(*args, **kwargs)
-        print("--- 処理終了 ---")
+        print("--- 後処理 ---")
         return result
     return wrapper
 
 @my_decorator
 def greet(name):
     print(f"こんにちは、{name}さん！")
-
-greet("太郎")
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 /* TODO: decorators my_decorator */
-function greet(name: string) {
+function greet(name: any) {
     console.log("こんにちは、{name}さん！")
 }
 
-greet("太郎")
